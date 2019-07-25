b0f4683b1175db3f037d370bc9fcce01a6133e84
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-07-26 03:22:40 +0530

    Added Room Database

---
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -0,0 +1,32 @@
+package com.example.roomwordssample;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class},version = 1,exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    // Define the DAOs that work with the database. Provide an abstract "getter" method for each @Dao
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating
+                            // if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
