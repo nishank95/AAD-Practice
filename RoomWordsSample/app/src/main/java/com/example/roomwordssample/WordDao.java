78e250998a2f9dd2d19cd68ccfa442e6da06a5ff
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-07-26 03:15:08 +0530

    Room Backend config

---
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordDao.java
@@ -0,0 +1,24 @@
+package com.example.roomwordssample;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    //  If you use a return value of type LiveData in your method description, Room generates all necessary code         to update the LiveData when the database is updated.
+    @Query("SELECT * FROM word_table ORDER BY word")
+    LiveData<List<Word>> getAllWords();
+
+}
