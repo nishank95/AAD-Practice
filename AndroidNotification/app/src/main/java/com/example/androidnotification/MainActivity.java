6d2b1685121ed823133fc1406139fa2aa2a92fab
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-07-27 00:29:05 +0530

    Create Notification

--- AndroidNotification/app/src/main/java/com/example/androidnotification/MainActivity.java
+++ AndroidNotification/app/src/main/java/com/example/androidnotification/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.androidnotification;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +13,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button_notify;
+    private static final int NOTIFICATION_ID = 0;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
 
@@ -23,9 +28,38 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+        createNotificationChannel();
+    }
+
+    //Create ane return a Notification builder object
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notifyBuilder =
+                new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                        .setContentTitle("You've been notified!")
+                        .setContentText("This is your notification text.")
+                        .setSmallIcon(R.mipmap.ic_android);
+        return notifyBuilder;
+    }
+
+    public void createNotificationChannel(){
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,"Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+
+            // Initial settings for notification channel
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 
     public void sendNotification(){
-
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
     }
 }
