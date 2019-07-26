c2ed6b4edaa5ece526c9f46b467078bd0d2fbdac
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-07-27 01:20:25 +0530

    Add Content Intent and Dismiss Notification

--- AndroidNotification/app/src/main/java/com/example/androidnotification/MainActivity.java
+++ AndroidNotification/app/src/main/java/com/example/androidnotification/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidnotification;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +15,11 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button_notify;
+    //Identifier for notification in a package
     private static final int NOTIFICATION_ID = 0;
+    //Identifier for notification channel in a package
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    //Notification Manager
     private NotificationManager mNotifyManager;
 
     @Override
@@ -31,13 +36,34 @@ public class MainActivity extends AppCompatActivity {
         createNotificationChannel();
     }
 
-    //Create ane return a Notification builder object
+    //Create and return a Notification builder object
     private NotificationCompat.Builder getNotificationBuilder(){
+
+        Intent intent = new Intent(this,MainActivity.class);
+        // By using a PendingIntent to communicate with another app, you are telling that app to execute
+        // some predefined code at some point in the future. It's like the other app can perform an
+        // action on behalf of your app.
+        PendingIntent notificationPendingIntent =  PendingIntent.getActivity(this,
+                NOTIFICATION_ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder notifyBuilder =
                 new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                         .setContentTitle("You've been notified!")
                         .setContentText("This is your notification text.")
-                        .setSmallIcon(R.mipmap.ic_android);
+                        .setSmallIcon(R.mipmap.ic_android)
+                        // Use the setContentIntent() method from the NotificationCompat.Builder class to set the content intent
+                        .setContentIntent(notificationPendingIntent)
+                        // Setting auto-cancel to true closes the notification when user taps on it.
+                        .setAutoCancel(true)
+                        // Note: This task is required for devices running Android 7.1 or lower, which
+                        // is most Android-powered devices and it's a best practice to provide backward compatibility and
+                        // support for lower-end devices.
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        // Set the sound, vibration, and LED-color pattern for your notification
+                        // if the user's device has an LED indicator to the default values.
+                        .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+
         return notifyBuilder;
     }
 
@@ -46,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
         if (android.os.Build.VERSION.SDK_INT >=
                 android.os.Build.VERSION_CODES.O) {
             // Create a NotificationChannel
-            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,"Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
 
             // Initial settings for notification channel
             notificationChannel.enableLights(true);
@@ -58,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Send the notification builded previously
     public void sendNotification(){
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
         mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
