31da79756e716fbf040ef9dbb21d0650f539e383
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-07-27 20:59:24 +0530

    Add Notification Action

--- AndroidNotification/app/src/main/java/com/example/androidnotification/MainActivity.java
+++ AndroidNotification/app/src/main/java/com/example/androidnotification/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.androidnotification;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -25,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     //Notification Manager
     private NotificationManager mNotifyManager;
+
+    private static final String ACTION_UPDATE_NOTIFICATION =
+            "com.example.androidnotification.ACTION_UPDATE_NOTIFICATION";
+    private NotificationReciever mReceiver = new NotificationReciever();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +63,13 @@ public class MainActivity extends AppCompatActivity {
                 cancelNotification();
             }
         });
+
         createNotificationChannel();
+        //Toggle Button states
         setNotificationButtonState(true, false, false);
+        // To receive the ACTION_UPDATE_NOTIFICATION intent, register your broadcast
+        // receiver in the onCreate() method
+        registerReceiver(mReceiver,new IntentFilter(ACTION_UPDATE_NOTIFICATION));
     }
 
     //Create and return a Notification builder object
@@ -71,18 +82,25 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent notificationPendingIntent =  PendingIntent.getActivity(this,
                 NOTIFICATION_ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
+        PendingIntent notificationDismissIntent =  PendingIntent.getActivity(this,
+                NOTIFICATION_ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder notifyBuilder =
                 new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                         .setContentTitle("You've been notified!")
                         .setContentText("This is your notification text.")
                         .setSmallIcon(R.mipmap.ic_android)
-                        // Use the setContentIntent() method from the NotificationCompat.Builder class to set the content intent
+                        // Use the setContentIntent() method from the NotificationCompat.Builder
+                        // class to set the content intent
                         .setContentIntent(notificationPendingIntent)
                         // Setting auto-cancel to true closes the notification when user taps on it.
                         .setAutoCancel(true)
+                        // Create another pending intent to let the app know that the user has
+                        // dismissed the notification, and toggle the button states accordingly.
+                        .setDeleteIntent(notificationDismissIntent)
                         // Note: This task is required for devices running Android 7.1 or lower, which
-                        // is most Android-powered devices and it's a best practice to provide backward compatibility and
-                        // support for lower-end devices.
+                        // is most Android-powered devices and it's a best practice to
+                        // provide backward compatibility support for lower-end devices.
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
                         // Set the sound, vibration, and LED-color pattern for your notification
                         // if the user's device has an LED indicator to the default values.
@@ -112,13 +130,25 @@ public class MainActivity extends AppCompatActivity {
 
     //Send the notification builded previously
     public void sendNotification(){
-        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
-        mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
-        setNotificationButtonState(false, true, true);
 
+        // create an Intent using the custom update action ACTION_UPDATE_NOTIFICATION
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        // Use getBroadcast() to get a PendingIntent. To make sure that this pending intent
+        // is sent and used only once, set FLAG_ONE_SHOT
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(this,
+                NOTIFICATION_ID,updateIntent,PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+
+        // Use the addAction() method to add an action to the NotificationCompat.Builder object
+        notifyBuilder.addAction(R.drawable.ic_update,"Update Notification",updatePendingIntent);
+        mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+        // Toggle Button state
+        setNotificationButtonState(false, true, true);
     }
 
     public void updateNotification() {
+
         Bitmap androidImage = BitmapFactory
                 .decodeResource(getResources(),R.drawable.mascot_1);
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
@@ -140,5 +170,25 @@ public class MainActivity extends AppCompatActivity {
         button_notify.setEnabled(isNotifyEnabled);
         button_update.setEnabled(isUpdateEnabled);
         button_cancel.setEnabled(isCancelEnabled);
+    }
+
+    public class NotificationReciever extends BroadcastReceiver{
+
+        public NotificationReciever(){
+
+        }
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Update the notification
+            updateNotification();
+        }
+
+    }
+
+    // To unregister your receive
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
