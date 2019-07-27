30cbf48c5080adc68f287084e59a9fc2e343206a
Nishank Sanjeev Samant <nishanksamant@gmail.com>
2019-07-28 00:27:42 +0530

    Implement

---
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.notificationscheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scheduleJob(){
+
+    }
+
+    public void cancelJobs(){
+
+    }
+
+}
---
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -0,0 +1,68 @@
+package com.example.notificationscheduler;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationJobService extends JobService {
+
+    private NotificationManager mNotifyManager;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    public void createNotificationChannel() {
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Job service notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription
+                    ("Notifications from Job Service");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+
+        //Create the notification channel
+        createNotificationChannel();
+
+        //Set up the notification content intent to launch the app when clicked
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class), PendingIntent                .FLAG_UPDATE_CURRENT);
+        // Construct and deliver a notification with the following attributes:
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder
+                (this,PRIMARY_CHANNEL_ID)
+                .setContentTitle("Job Service")
+                .setContentText("Your Job ran to completion!")
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_job_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+        mNotifyManager.notify(0,notificationBuilder.build());
+
+
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return true;
+    }
+}
